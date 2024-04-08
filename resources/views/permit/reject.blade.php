74bebcd30c9c17d11d70f308040d8048c73e19b9
hojinlee <infohojin@gmail.com>
2024-04-08 17:15:45 +0900

    reject

--- resources/views/permit/reject.blade.php
+++ resources/views/permit/reject.blade.php
@@ -1,19 +1,22 @@
-@extends('jinyauth::layouts.bootstrap')
-@section('content')
+<x-app>
+    <x-bootstrap>
+        <x-page-center>
+            <div class="text-center mt-4">
+                <h1 class="h2">관리자 페이지에 접속할 수 없습니다..</h1>
+                <p class="lead"></p>
+            </div>
 
-<div class="text-center mt-4">
-    <h1 class="h2">관리자만 접속이 가능합니다.</h1>
-    <p class="lead">관리자 페이지에 접속할 수 없습니다.</p>
-</div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="text-center">
+                        관리자만 접속이 가능합니다
+                    </div>
+                    <div class="text-center">
+                        권환이 없는 사용자가 지속적으로 접속을 시도하는 경우, 운영자에게 통보됩니다.
+                    </div>
 
-<div class="card">
-    <div class="card-body">
-
-        <div class="text-center">
-            권환이 없는 사용자가 지속적으로 접속을 시도하는 경우, 운영자에게 통보됩니다.
-        </div>
-
-    </div>
-</div>
-
-@endsection
+                </div>
+            </div>
+        </x-page-center>
+    </x-bootstrap>
+</x-app>
